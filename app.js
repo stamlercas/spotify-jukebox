06548966db0f67d90e4c0273f1f5385533787e2e
cstamler <cstamler@andrew.cmu.edu>
2021-02-06 15:50:00 -0500

    update request logging

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.set('spotifyPlayer', spotifyPlayer);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
